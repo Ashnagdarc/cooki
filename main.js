3f0a6bc07a255bdb67347c4e6596bc30e45e53f1
Daniel Samuel <46603289+Ashnagdarc@users.noreply.github.com>
2021-09-16 07:54:44 +0800

    Update main.js

--- main.js
+++ main.js
@@ -2,17 +2,17 @@ const searchForm = document.querySelector("form");
 const searchResultDiv = document.querySelector(".search-result");
 const container = document.querySelector(".container");
 let searchQuery = "";
-const APP_ID = "Use Your Own App ID Here";
-const APP_key = "Use Your Own App Key Here";
-// console.log(container)
+const APP_ID = "4c502aed";
+const APP_key = "9a3222b9fe5c26bb3bb8f7754ecb0f2d";
+
 searchForm.addEventListener("submit", (e) => {
   e.preventDefault();
   searchQuery = e.target.querySelector("input").value;
   fetchAPI();
 });
 
-async function fetchAPI() {
-  const baseURL = `https://api.edamam.com/search?q=${searchQuery}&app_id=${APP_ID}&app_key=${APP_key}&from=0&to=20`;
+async function fetchAPI (){
+  const baseURL = `https://api.edamam.com/search?q=${searchQuery}&app_id=${APP_ID}&app_key=${APP_key}&from=0&to=50`;
   const response = await fetch(baseURL);
   const data = await response.json();
   generateHTML(data.hits);
@@ -23,14 +23,13 @@ function generateHTML(results) {
   container.classList.remove("initial");
   let generatedHTML = "";
   results.map((result) => {
-    generatedHTML += `
-      <div class="item">
+    generatedHTML +=
+    `
+    <div class="item">
         <img src="${result.recipe.image}" alt="img">
         <div class="flex-container">
           <h1 class="title">${result.recipe.label}</h1>
-          <a class="view-btn" target="_blank" href="${
-            result.recipe.url
-          }">View Recipe</a>
+          <a class="view-btn" target="_blank" href="${result.recipe.url}">View Recipe</a>
         </div>
         <p class="item-data">Calories: ${result.recipe.calories.toFixed(2)}</p>
         <p class="item-data">Diet label: ${
@@ -39,26 +38,8 @@ function generateHTML(results) {
             : "No Data Found"
         }</p>
         <p class="item-data">Health labels: ${result.recipe.healthLabels}</p>
-      </div>
+    </div>
     `;
   });
   searchResultDiv.innerHTML = generatedHTML;
 }
-
-// *********************
-// This Code is for only the floating card in right bottom corner
-// **********************
-
-const WebCifarIcon = document.querySelector("#webCifar-icon");
-const WebCifarEl = document.querySelector("#webCifar");
-const close = WebCifarEl.querySelector(".close");
-const youtubeLink = document.querySelector(".youtubeLink");
-
-WebCifarIcon.addEventListener("click", () => {
-  WebCifarEl.classList.add("active");
-});
-close.addEventListener("click", () => {
-  WebCifarEl.classList.remove("active");
-});
-
-youtubeLink.setAttribute("href", "https://youtu.be/x8EY0BlhPGk");
